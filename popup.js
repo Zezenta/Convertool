9b1d224e10e10677cc0a777a3ecebfb449050252
José Miguel <101609455+Zezenta@users.noreply.github.com>
2023-08-29 23:42:20 -0500

    popup.js doesnt fetch JSON anymore, it contains it

--- popup.js
+++ popup.js
@@ -1,9 +1,39 @@
-fetch("measures.json")
-  .then(response => response.json()) // Parse the response as JSON
-  .then(data => {
-    const unities = data; // Now you have the parsed JSON data
-    console.log(unities);
-  })
-  .catch(error => {
-    console.error("Error fetching JSON:", error);
-  });
+const measures = `{
+  "meter":{
+      "convertRatio": {
+          "foot": 3.28084,
+          "in": 39.3701,
+          "yard": 1.09361
+      }
+  },
+  "foot":{
+      "convertRatio": {
+          "meter": 0.3048,
+          "in": 12,
+          "yard": 0.333333
+      }
+  },
+  "kilometer":{
+      "convertRatio": {
+          "meter": 1000,
+          "mile": 0.621371,
+          "foot":  3280.84,
+          "yard": 1094
+      }
+  },
+  "mile":{
+      "convertRatio": {
+          "kilometer": 1.609,
+          "meter": 1609,
+          "foot": 5280,
+          "yard": 1760
+      }
+  },
+  "inch":{
+      "convertRatio": {
+          "centimeter": 2.54,
+          "meter": 0.0254,
+          "foot": 0.0833333
+      }
+  }
+}`;
